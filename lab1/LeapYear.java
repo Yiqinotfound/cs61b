c019fb90c8fe6874700fd20464bd81857ad8c10c
yiqi <1214771220@qq.com>
2023-02-04 16:57:48 +0800

    first commit of LeapYear

--- lab1/LeapYear.java
+++ lab1/LeapYear.java
@@ -1,16 +1,28 @@
-/** Class that determines whether or not a year is a leap year.
- *  @author YOUR NAME HERE
+/**
+ * Class that determines whether or not a year is a leap year.
+ * 
+ * @author Yiqi
  */
 public class LeapYear {
 
-    /** Calls isLeapYear to print correct statement.
-     *  @param  year to be analyzed
+    /**
+     * Calls isLeapYear to print correct statement.
+     * 
+     * @param year to be analyzed
      */
     private static void checkLeapYear(int year) {
         if (isLeapYear(year)) {
             System.out.printf("%d is a leap year.\n", year);
         } else {
             System.out.printf("%d is not a leap year.\n", year);
+        }
+    }
+
+    public static boolean isLeapYear(int year) {
+        if ((year % 4 == 0 && year % 100 != 0) || year % 400 == 0) {
+            return true;
+        } else {
+            return false;
         }
     }
 
@@ -30,4 +42,3 @@ public class LeapYear {
         }
     }
 }
-
